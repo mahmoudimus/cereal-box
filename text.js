7c93a1bc0c391f0ea2b9c4380792504c6cafe329
Mahmoud Abdelkader <mahmoudimus@users.noreply.github.com>
2018-03-09 01:45:16 -0800

    Update text.js

--- text.js
+++ text.js
@@ -5,5 +5,11 @@ exports.encodeText = string => new Uint8Array(
 // The last argument for the Uint16Array constructor is not the byte-length to use as the new range,
 // but the intended length of the u16 array.  Because each element in a u16 array consumes two bytes,
 // the byte-length must be divided by two.
-exports.decodeText = u8 =>
-  String.fromCharCode(...(new Uint16Array(u8.buffer, u8.byteOffset, u8.byteLength / 2)));
+exports.decodeText = u8 => {
+    var data = new Uint16Array(u8.byteLength / 2);
+    for (var i = 0, j = 0; j < u8.byteLength; i++, j+=2) {
+        data[i] = u8.buffer[j + 1] | u8.buffer[j] << 8;
+    }
+    return String.fromCharCode.apply(null, data);
+    //   String.fromCharCode(...(new Uint16Array(u8.buffer, u8.byteOffset, u8.byteLength / 2)));
+}
