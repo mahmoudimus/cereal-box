25b1a8594cb3e91fc28b41eb67aef8cccd888520
Mahmoud Abdelkader <mahmoudimus@users.noreply.github.com>
2018-03-09 08:57:01 -0800

    Update text.js

--- text.js
+++ text.js
@@ -2,15 +2,24 @@ exports.encodeText = string => new Uint8Array(
   new Uint16Array(Array.prototype.map.call(string, (_, idx) => string.charCodeAt(idx))).buffer
 );
 
+var returnSingle = function (input) {
+    if (input.length === 1) {
+        return input[0];
+    }
+    return input;
+}
+
+var getUint8Part = function (buffer, byteOffset, length, bytesPerInteger) {
+    var srcU8 = new Uint8Array(buffer, byteOffset, length * bytesPerInteger);
+    var dstU8 = new Uint8Array(length * bytesPerInteger);
+    dstU8.set(srcU8);
+    return dstU8.buffer;
+}
+
 // The last argument for the Uint16Array constructor is not the byte-length to use as the new range,
 // but the intended length of the u16 array.  Because each element in a u16 array consumes two bytes,
 // the byte-length must be divided by two.
 exports.decodeText = u8 => {
-    var data = new Uint16Array(u8.byteLength / 2);
-    for (var i = 0, j = 0; j < u8.byteLength; i++, j+=2) {
-         data[i] = u8[j + 1] | u8[j] << 8;
-    }
-//    console.log(data);
-    return String.fromCharCode.apply(null, data);
-    //String.fromCharCode(...(new Uint16Array(u8.buffer, u8.byteOffset, u8.byteLength / 2)));
-}
+    var dstU8buffer = getUint8Part(u8.buffer, u8.byteOffset, u8.byteLength, 2);
+    return String.fromCharCode(...(new Uint16Array(dstU8buffer, 0, length)));
+};
